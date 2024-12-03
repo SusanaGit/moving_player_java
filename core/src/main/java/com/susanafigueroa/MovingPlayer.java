6354a0b9a29ad3b8aec36022228a18327f7afaa8
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-03 19:43:49 +0100

    MovingPlayer extends Game

--- core/src/main/java/com/susanafigueroa/MovingPlayer.java
+++ core/src/main/java/com/susanafigueroa/MovingPlayer.java
@@ -1,14 +1,12 @@
 package com.susanafigueroa;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class MovingPlayer extends ApplicationAdapter {
+public class MovingPlayer extends Game {
     private SpriteBatch batch;
     private Texture image;
 
