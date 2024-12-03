e5c5f798ca9c05acac5047c6fbcb4eef4c35c454
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-03 22:06:04 +0100

    add background

--- core/src/main/java/com/susanafigueroa/MovingPlayer.java
+++ core/src/main/java/com/susanafigueroa/MovingPlayer.java
@@ -1,25 +1,32 @@
 package com.susanafigueroa;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MovingPlayer extends Game {
+
+    // main component for driving things on the screen
     private SpriteBatch batch;
     private Texture image;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("logo.png");
+        image = new Texture("Game BG.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        int screenWidth = Gdx.graphics.getWidth();
+        int screenHeight = Gdx.graphics.getHeight();
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 0, 0, screenWidth, screenHeight);
         batch.end();
     }
 
