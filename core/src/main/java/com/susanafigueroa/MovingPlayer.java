14a8cbe34be20e1bd5b2e7b023c91324430f63c8
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-04 10:41:55 +0100

    using OrthographicCamera and StretchViewport to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes

--- core/src/main/java/com/susanafigueroa/MovingPlayer.java
+++ core/src/main/java/com/susanafigueroa/MovingPlayer.java
@@ -1,10 +1,13 @@
 package com.susanafigueroa;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+
+import helpers.GameInfo;
 
 public class MovingPlayer extends Game {
 
@@ -13,8 +16,23 @@ public class MovingPlayer extends Game {
     private Texture image;
     private Texture turtle;
 
+    // I want to use the WIDTH and the HEIGHT of the GameInfo to declare thing positions and sizes
+    // OrthographicCamera -> defines 2D perspective of the game
+    private OrthographicCamera camera;
+
+    // StretchViewport -> to maintain a fixed aspect ratio
+    private StretchViewport viewport;
+
     @Override
     public void create() {
+        // initialize camera and viewport
+        camera = new OrthographicCamera();
+        viewport = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, camera);
+
+        // set the position of the camera in the middle of the screen
+        camera.position.set(GameInfo.WIDTH/2f , GameInfo.HEIGHT/2f, 0);
+        camera.update();
+
         batch = new SpriteBatch();
         image = new Texture("Game BG.png");
         turtle = new Texture("turtle.png");
@@ -24,18 +42,24 @@ public class MovingPlayer extends Game {
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
-        int screenWidth = Gdx.graphics.getWidth();
-        int screenHeight = Gdx.graphics.getHeight();
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
         batch.begin();
-        batch.draw(image, 0, 0, screenWidth, screenHeight);
-        batch.draw(turtle, (float) screenWidth /2, 0, 200, 200);
+        batch.draw(image, 0, 0, GameInfo.WIDTH, GameInfo.HEIGHT);
+        batch.draw(turtle, (float) GameInfo.WIDTH /2, 0, 200, 200);
         batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
     }
 
     @Override
     public void dispose() {
         batch.dispose();
         image.dispose();
+        turtle.dispose();
     }
 }
--- core/src/main/java/helpers/GameInfo.java
+++ core/src/main/java/helpers/GameInfo.java
@@ -3,7 +3,7 @@ package helpers;
 // contains some information about the game
 public class GameInfo {
 
-    public static int WIDTH = 640;
-    public static int HEIGHT = 480;
+    public static int WIDTH = 480;
+    public static int HEIGHT = 800;
 
 }
