0ea9573de7726c95b2dd11e5bf3bbf1b4e58f349
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-12-05 19:11:24 +0100

    implement createBody method

--- core/src/main/java/player/Player.java
+++ core/src/main/java/player/Player.java
@@ -2,8 +2,11 @@ package player;
 
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class Player extends Sprite {
@@ -17,7 +20,31 @@ public class Player extends Sprite {
         setPosition(x - getWidth() / 2, y - getHeight() / 2);
     }
 
-    public void move (Vector2 direction, float speed) {
-        setPosition(getX() + direction.x * speed, getY() + direction.y * speed);
+    public void createBody() {
+        BodyDef bodyDef = new BodyDef();
+        // static -> not afected by other forces
+        // kinematic -> not afected by gravity, afected by other forces
+        // DynamicBody -> afected by gravity and other forces
+        bodyDef.type = BodyDef.BodyType.DynamicBody;
+
+        // posición inicial, la misma que el player
+        bodyDef.position.set(getX(), getY());
+
+        // body ahora está en el world
+        body = world.createBody(bodyDef);
+
+        // defino rectángulo
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(getWidth() / 2, getHeight() / 2);
+
+        // defino las propiedades físicas del body
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = shape;
+        fixtureDef.density = 1;
+
+        // fijo el body al rectángulo shape
+        Fixture fixture = body.createFixture(fixtureDef);
+
+        shape.dispose();
     }
 }
